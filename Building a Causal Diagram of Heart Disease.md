6568df622768cdee142503bc9e9aa726acfb08bb
CodeRonNor <ronh93@gmail.com>
2020-11-03 18:10:25 +0100

    new structure

---
+++ Building a Causal Diagram of Heart Disease.md
@@ -0,0 +1,10 @@
+# Building a Causal Diagram of Heart Disease
+
+| $t_0$: determined at birth | $t_1$: day to day stuff | $t_2$: symptoms         | $t_3$: measurements at the doc | $t_4$: results of measurements | $t_5$     |
+| -------------------------- | ----------------------- | ----------------------- | ------------------------------ | ------------------------------ | --------- |
+| sex                        | Avg_exercise_week       | chest pain              | Rest_ecg                       | St depression                  | Diagnosis |
+| age                        | Body_mass_index         | exercise Induced angina | Max_heart_rate                 | st slope                       |           |
+| thalassemia                | Avg_smoking_week        |                         | Rest_blood_pressure            | coloured arteries              |           |
+|                            | Avg_drinking_per_week   |                         | cholesterol                    |                                |           |
+|                            |                         |                         | fasting blood sugar            |                                |           |
+
--- heart_disease_network.R
+++ heart_disease_network.R
@@ -1,51 +1,75 @@
 library(devtools)
 library(dagitty)
+library( bayesianNetworks )
+library(lavaan)
+#setwd("~/Documents/Artificial Intelligence/Master/1. Year/1. Semester/Bayesian Networks/heart-disease")
 
-net <- dagitty('dag {
-  bb="0,0,1,1"
-  ST_depression [pos="0.371,0.925"]
-  ST_slope [pos="0.378,0.805"]
-  age [pos="0.822,0.078"]
-  chest_pain [pos="0.391,0.347"]
-  cholesterol [pos="0.681,0.246"]
-  coloured_arteries [pos="0.730,0.615"]
-  diagnosis [pos="0.625,0.929"]
-  exercise_induced_angina [pos="0.185,0.568"]
-  fasting_blood_sugar [pos="0.877,0.571"]
-  max_heart_rate [pos="0.122,0.079"]
-  rest_blood_press [pos="0.343,0.639"]
-  rest_ecg [pos="0.107,0.790"]
-  sex [pos="0.563,0.145"]
-  thalassemia [pos="0.215,0.291"]
-  ST_depression -> diagnosis
-  ST_slope -> diagnosis
-  age -> cholesterol
-  age -> fasting_blood_sugar
-  age -> max_heart_rate
-  chest_pain -> diagnosis
-  chest_pain -> exercise_induced_angina
-  cholesterol -> chest_pain
-  cholesterol -> coloured_arteries
-  cholesterol -> diagnosis
-  cholesterol -> max_heart_rate
-  cholesterol -> rest_blood_press
-  coloured_arteries -> diagnosis
-  exercise_induced_angina -> rest_ecg
-  fasting_blood_sugar -> cholesterol
-  fasting_blood_sugar -> diagnosis
-  max_heart_rate -> rest_ecg
-  rest_blood_press -> diagnosis
-  rest_blood_press -> rest_ecg
-  rest_ecg -> ST_depression
-  rest_ecg -> ST_slope
-  sex -> cholesterol
-  sex -> max_heart_rate
-  thalassemia -> exercise_induced_angina
-  thalassemia -> max_heart_rate
-  thalassemia -> rest_blood_press
-  }')
 
-plot(net)
+g <- dagitty('dag {
+bb="0,0,1,1"
+ST_depression [pos="0.711,0.156"]
+ST_slope [pos="0.713,0.501"]
+age [pos="0.068,0.464"]
+chest_pain [pos="0.324,0.253"]
+cholesterol [pos="0.526,0.625"]
+coloured_arteries [pos="0.709,0.717"]
+diagnosis [pos="0.961,0.424"]
+exercise_induced_angina [pos="0.320,0.611"]
+fasting_blood_sugar [pos="0.534,0.843"]
+max_heart_rate [pos="0.530,0.232"]
+rest_blood_press [pos="0.526,0.410"]
+rest_ecg [pos="0.526,0.064"]
+sex [pos="0.068,0.230"]
+thalassemia [pos="0.068,0.716"]
+ST_depression -> diagnosis
+ST_slope -> diagnosis
+age -> chest_pain
+age -> cholesterol
+age -> exercise_induced_angina
+age -> fasting_blood_sugar
+age -> max_heart_rate
+age -> rest_blood_press
+age -> rest_ecg
+chest_pain -> cholesterol
+chest_pain -> diagnosis
+chest_pain -> max_heart_rate
+chest_pain -> rest_blood_press
+chest_pain -> rest_ecg
+cholesterol -> coloured_arteries
+cholesterol -> diagnosis
+coloured_arteries -> diagnosis
+exercise_induced_angina -> cholesterol
+exercise_induced_angina -> max_heart_rate
+exercise_induced_angina -> rest_blood_press
+exercise_induced_angina -> rest_ecg
+fasting_blood_sugar -> diagnosis
+max_heart_rate -> ST_depression
+max_heart_rate -> ST_slope
+max_heart_rate -> diagnosis
+rest_blood_press -> ST_depression
+rest_blood_press -> ST_slope
+rest_blood_press -> coloured_arteries
+rest_blood_press -> diagnosis
+rest_ecg -> ST_depression
+rest_ecg -> ST_slope
+sex -> chest_pain
+sex -> cholesterol
+sex -> fasting_blood_sugar
+sex -> max_heart_rate
+sex -> rest_blood_press
+sex -> rest_ecg
+thalassemia -> chest_pain
+thalassemia -> exercise_induced_angina
+thalassemia -> max_heart_rate
+thalassemia -> rest_blood_press
+thalassemia -> rest_ecg
+}
+')
 
-data <- read.csv("Data/heart_disease_dataset.csv")
-head(data)
+plot(g)
+
+paths(g, "age", "diagnosis")
+
+d <- read.csv("data/processed_cleveland.csv", header=F, sep=',')
+
+impliedConditionalIndependencies( g )
